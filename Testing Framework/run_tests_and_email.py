1c5328cc540f915f8fc9386e1a0797fc899a4416
Neil Balch <neilbalch@gmail.com>
2019-08-15 14:25:30 -0700

    Add better comments to run_tests_and_email.py

--- Testing Framework/run_tests_and_email.py
+++ Testing Framework/run_tests_and_email.py
@@ -7,13 +7,18 @@ import example_geom_module as geom
 import example_path_module as path
 import example_solid_model_module as solid_model
 
+#
+# Reference for sending email in Python:
+# https://www.youtube.com/watch?v=JRCJ6RtE3xU
+#
+
 recipients = [
     "neilbalch@gmail.com",
     # "gdmaher@stanford.edu"
 ]
 
 # https://stackoverflow.com/a/57449397/3339274
-def flatten(nested_list):
+def flatten_multidimensional_list(nested_list):
     """ Return a list after transforming the inner lists
         so that it's a 1-D list.
 
@@ -28,10 +33,11 @@ def flatten(nested_list):
         if not isinstance(l, list):
             res += [l]
         else:
-            res += flatten(l)
+            res += flatten_multidimensional_list(l)
 
     return(res)
 
+# Get 2D list of raw output strings ([test module][lines]).
 raw_output = []
 raw_output.append(contour.contour_tests(colors=False))
 raw_output.append(geom.geom_tests(colors=False))
@@ -39,33 +45,41 @@ raw_output.append(geom.geom_tests(colors=False))
 raw_output.append(path.path_tests(colors=False))
 raw_output.append(solid_model.solid_model_tests(colors=False))
 
-flattened_output = flatten(raw_output)
-single_line_output = ""
+# Flatten the 2D list ([lines]).
+flattened_output = flatten_multidimensional_list(raw_output)
+
+# Create string for the plain text message.
+plain_text_msg = ""
 for line in flattened_output:
-  single_line_output += line
-  single_line_output += "\n"
+  plain_text_msg += line
+  plain_text_msg += "\n"
 
-message_contains_fail = False
+tests_contain_fail = False
 
-# Make deep copy of flattened output.
+# Make deep copy of flattened output and format it for email HTML.
 formatted_output = flattened_output[:]
 for i in range(len(formatted_output)):
+    # If a test failed, record that for display in the subject line.
     if "[ FAIL ]" in formatted_output[i]:
-        message_contains_fail = True
+        tests_contain_fail = True
 
+    # Color code the PASS and FAIL thingies
     formatted_output[i] = formatted_output[i].replace("[ PASS ]",
                           "[ <span style=\"color:Green\">PASS</span> ]")
     formatted_output[i] = formatted_output[i].replace("[ FAIL ]",
                           "[ <span style=\"color:Red\">FAIL</span> ]")
+    # Apply header tags to the test module summary lines.
     if "Results for" in formatted_output[i]:
         formatted_output[i] = "<h3>" + formatted_output[i] + "</h3>"
     if "tests completed:" in formatted_output[i]:
         formatted_output[i] = "<h4>" + formatted_output[i] + "</h4>"
+    # Replace string \n and \t chars with HTML equivalents.
     formatted_output[i] = formatted_output[i].replace("\n", "<br>")
     formatted_output[i] = formatted_output[i].replace("\t", "&#9;")
+    # Add line break to the end of every line.
     formatted_output[i] += "<br>"
 
-print(formatted_output)
+# print(formatted_output)
 
 # Format of ./credentials.file, verbatim:
 # [GMAIL ACCOUNT EMAIL ADDRESS]
@@ -75,17 +89,17 @@ with open("credentials.file", "r") as file:
     GMAIL_ADDRESS = credentials[0]
     GMAIL_PASSWD = credentials[1]
 
-# https://stackoverflow.com/a/48283832/3339274
+# Datetime: https://stackoverflow.com/a/48283832/3339274
 msg = EmailMessage()
-msg["Subject"] = ("[CI " + ("FAIL" if message_contains_fail else "PASS")
+msg["Subject"] = ("[CI " + ("FAIL" if tests_contain_fail else "PASS")
                  + "] SimVascular Python API Unit Test Report (" + ctime() + ")")
 msg["From"] = GMAIL_ADDRESS
 msg["To"] = recipients
 
 # Add plain-text version.
-msg.set_content(single_line_output)
+msg.set_content(plain_text_msg)
 
-# Add HTML version.
+# Add HTML version of message.
 msg.add_alternative("""\
 <!DOCTYPE html>
 <html>
